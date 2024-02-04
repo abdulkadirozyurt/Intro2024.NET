dcca0d7dc92b259ac49d4167cba679d83374b63b
Abdulkadir Özyurt <abdulkadir.ozyurt@outlook.com>
2024-02-04 22:20:47 +0300

    condition block and loops, arrays

--- Intro2024C#/Program.cs
+++ Intro2024C#/Program.cs
@@ -7,10 +7,59 @@ string message1 = "Krediler";
 int term = 12;
 double amount = 100000.5;
 
-bool isAuthenticated = true;
+bool isAuthenticated = false;
 
 
 // variables ---> camelCase
+
+
+
+
+// ************ conditions ************
+
+
+if (isAuthenticated)
+{
+    Console.WriteLine("Buton --> Hoşgeldiniz...");
+}
+else
+{
+    Console.WriteLine("Lütfen giriş yapınız..");
+}
+
+
+
+// ************ arrays ************
+
+string[] loans = { "Kredi1", "Kredi2", "Kredi3", "Kredi4", "Kredi5", };
+string[] loans2 = new string[5];
+loans2[0] = "Kredi1";
+
+string[] loans3 = new string[]
+{
+    "kredi1",
+    "Kredi2"
+};
+
+                                    // increment
+for (int i = 0; i < loans.Length; i++)
+{
+    Console.WriteLine(loans[i]);
+}
+
+
+
+
+
+
+
+
+
+
+
+Console.WriteLine("Kod bitti");
+
+
 
 
 
