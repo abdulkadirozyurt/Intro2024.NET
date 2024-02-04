ef4053c9a4b0ac76bd134fe6a3cbc29f363be481
Abdulkadir Özyurt <abdulkadir.ozyurt@outlook.com>
2024-02-04 21:21:55 +0300

    variables

--- Intro2024C#/Program.cs
+++ Intro2024C#/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+
+string message1 = "Krediler";
+int term = 12;
+double amount = 100000.5;
+
+bool isAuthenticated = true;
+
+
+// variables ---> camelCase
+
+
+
+
